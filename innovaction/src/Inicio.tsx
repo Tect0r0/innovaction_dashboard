c8bb2900c5995da65b3938786f6c5d91dc945329
Gerardo Leiva Diaz <a01198479@tec.mx>
2024-04-04 13:08:09 -0600

    Setup adicional

--- innovaction/src/Inicio.tsx
+++ innovaction/src/Inicio.tsx
@@ -1,7 +1,15 @@
 export default function Inicio(){
     return (
         <div className="page">
-            <h1>Inicio</h1>
+            <h1>¿Quiénes somos?</h1>
+            <p>El innovaction es una inovacion</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
         </div>
     )
 }
