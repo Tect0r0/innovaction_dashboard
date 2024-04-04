b0a8ef674e9a530a1b59a7b4de60bfb5a07860eb
Gerardo Leiva Diaz <a01198479@tec.mx>
2024-04-04 14:38:59 -0600

    Calendario inicial
    Implementacion inicial del calendario (copiado del inspect element de canvas [está muy cabrón])

--- innovaction/src/Calendario.tsx
+++ innovaction/src/Calendario.tsx
@@ -5,8 +5,249 @@ export default function Calendario(){
             <div className="calendar_container">
                 <div className="cal-month-view">
                     <table>
-                        <thead ></thead>
-                        <tbody></tbody>
+                        <thead className="cal-head">
+                            <tr>
+                                <td className="cal-header">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>a</th> <th>b</th> <th>c</th> <th>d</th> <th>e</th> <th>f</th> <th>g</th> 
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody className="cal-body">
+                            <tr>
+                                <td className="cal-content">
+                                    <div className="date-container">
+                                        <div className="date-grid">
+                                            <div className="cal-week">
+                                                <div className="cal-bg">
+                                                    <table>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-today"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="cal-skeleton">
+                                                    <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <td className="cal-day-number-past">1</td>
+                                                                <td className="cal-day-number-past">2</td>
+                                                                <td className="cal-day-number-past">3</td>
+                                                                <td className="cal-day-number-today">4</td>
+                                                                <td className="cal-day-number-future">5</td>
+                                                                <td className="cal-day-number-future">6</td>
+                                                                <td className="cal-day-number-future">7</td>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <tbody></tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                            <div className="cal-week">
+                                                <div className="cal-bg">
+                                                    <table>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-today"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="cal-skeleton">
+                                                    <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <td className="cal-day-number-past">8</td>
+                                                                <td className="cal-day-number-past">8</td>
+                                                                <td className="cal-day-number-past">10</td>
+                                                                <td className="cal-day-number-today">11</td>
+                                                                <td className="cal-day-number-future">12</td>
+                                                                <td className="cal-day-number-future">13</td>
+                                                                <td className="cal-day-number-future">14</td>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <tbody></tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                            <div className="cal-week">
+                                                <div className="cal-bg">
+                                                    <table>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-today"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="cal-skeleton">
+                                                    <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <td className="cal-day-number-past">15</td>
+                                                                <td className="cal-day-number-past">16</td>
+                                                                <td className="cal-day-number-past">17</td>
+                                                                <td className="cal-day-number-today">18</td>
+                                                                <td className="cal-day-number-future">19</td>
+                                                                <td className="cal-day-number-future">20</td>
+                                                                <td className="cal-day-number-future">21</td>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <tbody></tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                            <div className="cal-week">
+                                                <div className="cal-bg">
+                                                    <table>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-today"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="cal-skeleton">
+                                                    <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <td className="cal-day-number-past">22</td>
+                                                                <td className="cal-day-number-past">23</td>
+                                                                <td className="cal-day-number-past">24</td>
+                                                                <td className="cal-day-number-today">25</td>
+                                                                <td className="cal-day-number-future">26</td>
+                                                                <td className="cal-day-number-future">27</td>
+                                                                <td className="cal-day-number-future">28</td>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <tbody></tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                            <div className="cal-week">
+                                                <div className="cal-bg">
+                                                    <table>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-past"></td>
+                                                                <td className="cal-day-today"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                                <td className="cal-day-future"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="cal-skeleton">
+                                                    <table>
+                                                        <thead>
+                                                            <tr>
+                                                                <td className="cal-day-number-past">29</td>
+                                                                <td className="cal-day-number-past">30</td>
+                                                                <td className="cal-day-number-past">1</td>
+                                                                <td className="cal-day-number-today">2</td>
+                                                                <td className="cal-day-number-future">3</td>
+                                                                <td className="cal-day-number-future">4</td>
+                                                                <td className="cal-day-number-future">5</td>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                                <td className="cal-day-event"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <tbody></tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
