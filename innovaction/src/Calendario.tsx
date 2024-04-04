479b556a9a516fe40d08e480d5e147fca9ce896e
Gerardo Leiva Diaz <a01198479@tec.mx>
2024-04-04 13:56:07 -0600

    Diseño y acomodo inicial

--- innovaction/src/Calendario.tsx
+++ innovaction/src/Calendario.tsx
@@ -2,6 +2,14 @@ export default function Calendario(){
     return (
         <div className="page">
             <h1>Calendario</h1>
+            <div className="calendar_container">
+                <div className="cal-month-view">
+                    <table>
+                        <thead ></thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
