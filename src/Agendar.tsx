4a1356f1b27bee12fa1bb01422341661ae0757fd
A01198693 <isaacrojassosa@gmail.com>
2024-05-08 09:45:54 -0600

    Cambios

--- src/Agendar.tsx
+++ src/Agendar.tsx
@@ -8,7 +8,7 @@ export default function Agendar(){
     return(
         <div className='container'>
             <div className='headLine'>
-                 <h1>Agenda tu evento </h1>
+                 <h1>Agendar evento </h1>
             </div>
             <br />
 
@@ -20,17 +20,17 @@ export default function Agendar(){
 
                     <div className='form-field'>
                         <label htmlFor="nombre">Nombre completo:</label>
-                        <input id="nombre" type="text" name="nombre" placeholder="Nombre" required/>
+                        <input id="nombre" type="text" name="nombre" placeholder="Nombre" required autoComplete='off'/>
                     </div>
 
                     <div className='form-field'>
-                        <label htmlFor="correo">Correo institucional:</label>
-                        <input id="correo" type="email" name="correo" placeholder="Correo" required/>
+                        <label htmlFor="correo">Correo o teléfono:</label>
+                        <input id="corrtel" type="text" name="corrtel" placeholder="Correo / Teléfono" required autoComplete='off'/>
                     </div>
 
                     <div className='form-field'>
-                        <label htmlFor="grupo">Grupo estudiantil:</label>
-                        <input id="grupo" type="text" name="grupo" placeholder="Grupo" required/>
+                        <label htmlFor="grupo">Asociación:</label>
+                        <input id="grupo" type="text" name="grupo" placeholder="Grupo" required autoComplete='off'/>
                     </div>
                
                     <div className='form-field'>
@@ -39,37 +39,49 @@ export default function Agendar(){
                             <option value="" selected disabled hidden>-Eliga un espacio-</option>
                             <option id="fractal" value="fractal">Fractal</option>
                             <option id="esferas" value="esferas">Esferas</option>
+                            <option id="misenina" value="misenina">Misenina</option>
                             <option id="makerspace" value="makerspace">Maker Space</option>
                             <option id="sparring" value="sparring">Sparring</option>
+                            <option id="todo" value="todo">Todo el Innovaction</option>
                             <option id="otro" value="otro">Otro</option>
                         </select>
                     </div>
 
-                  <div className='form-field'>
-                    <label htmlFor="tipo">Tipo de evento:</label>
-                        <select id="tipo" name="tipo" required onChange={(e) => setEvento(e.target.value)}>
-                            <option value="" selected disabled hidden>-Eliga un tipo de evento-</option>
-                            <option id="conferencai" value="conferencia">Conferencia</option>
-                            <option id="taller" value="taller">Taller</option>
-                            <option id="convivio" value="convivio">Convivio Social</option>
-                            <option id="clase" value="clase">Clase</option>
-                            <option id="otro" value="otro">Otro</option>
+                    <div className='form-field'>
+                        <label htmlFor="tipo">Tipo de evento:</label>
+                            <select id="tipo" name="tipo" required onChange={(e) => setEvento(e.target.value)}>
+                                <option value="" selected disabled hidden>-Eliga un tipo de evento-</option>
+                                <option id="conferencai" value="conferencia">Conferencia</option>
+                                <option id="taller" value="taller">Taller</option>
+                                <option id="convivio" value="convivio">Convivio Social</option>
+                                <option id="clase" value="clase">Clase</option>
+                                <option id="otro" value="otro">Otro</option>
+                            </select>
+                    </div>
+
+                    <div className='form-field'>
+                    <label htmlFor="fecha">Fecha y hora:</label>
+                        <input id="fecha" type="datetime-local" name="fecha" min="2024-01-01T00:00" max="2024-12-31T23:59" step="1"/>
+                    </div>
+
+                    <div className='form-field'>
+                        <label>Nivel de ruido:</label>
+                        <select id="ruido" name="ruido" required>
+                            <option value="" selected disabled hidden>-Eliga un nivel de ruido-</option>
+                            <option id="bajo" value="bajo">Bajo</option>
+                            <option id="medio" value="medio">Medio</option>
+                            <option id="alto" value="alto">Alto</option>
                         </select>
-                  </div>
-
-                  <div className='form-field'>
-                  <label htmlFor="fecha">Fecha y hora:</label>
-                    <input id="fecha" type="datetime-local" name="fecha" min="2024-01-01T00:00" max="2024-06-30T23:59" step="1"/>
-                  </div>
-
-                  <br />
+                    </div>
 
                     </div>
                     <div className='form-field'>
                         <label htmlFor="motivo">Información adicional:</label>
-                        <textarea id="info" name="info" placeholder="Breve descripción (obligatorio si elegiste 'otro')" rows={4} cols={30} required={espacio === 'otro' || evento === 'otro'}></textarea><br/>
+                        <textarea id="info" name="info" placeholder="Breve descripción (obligatorio si elegiste 'otro')" rows={4} cols={30} required={espacio === 'otro' || evento === 'otro'} autoComplete='off'></textarea><br/>
                     </div>
                     <br />
+
+                    
                     
                     <button className="submit" type="submit">Enviar</button>
                     <br />
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
 import Body from './Body'
 import Logo from './assets/innovaction1-t.png'
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 export default function App() {
   return (
     <>
-      <SignedIn>  {/*Si el usuario está logueado, muestra el contenido*/}
-        <header>
-          <div className="title-card">
-            <img className="logo" src={Logo} alt="Logo" />
-            <h1 className="title">Innovaction Gym - Event Dashboard</h1>
-            <UserButton />
-          </div>
-        </header>
-        <Body />
-      </SignedIn>
-
-      <SignedOut>
-        <header>
-          <div className="title-card">
-            <img className="logo" src={Logo} alt="Logo" />
-            <h1 className="title">Innovaction Gym - Event Dashboard</h1>
-            <SignInButton />
-          </div>
-        </header>
-      </SignedOut>
+      <header>
+        <div className="title-card">
+          <img className="logo" src={Logo} alt="Logo" />
+          <h1 className="title">Innovaction Gym - Event Dashboard</h1>
+          <SignedOut><SignInButton /></SignedOut> {/* <SignedOut>:  Si el usuario no está logueado */}
+          <UserButton />
+        </div>
+      </header>
+      <Body />
     </>
   )
 }
