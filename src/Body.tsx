e4c300a317324a083065dbdfc4f74305a2c28990
A01198693 <156484215+IsaacRoSosa@users.noreply.github.com>
2024-06-21 08:14:24 -0600

    Mapa
    Mapa añadido al popUp

--- src/Body.tsx
+++ src/Body.tsx
@@ -5,8 +5,9 @@ import Inicio from './Inicio'
 import Calendario from './Calendario'
 import Agendar from './Agendar'
 import Contacto from './Contacto'
+import Reporte from './Reporte'
 
-type PageName = 'Inicio' | 'Calendario' | 'Agendar' | 'Contacto'; // Nombres de las paginas
+type PageName = 'Inicio' | 'Calendario' | 'Agendar' | 'Contacto' | 'Reporte' ; // Nombres de las paginas
 
 export default function Body() {
     const initialPage = localStorage.getItem("page") as PageName || "Inicio"; // Obtenemos la pagina guardada en el local storage
@@ -20,6 +21,7 @@ export default function Body() {
       "Calendario": <Calendario />,
       "Agendar": <Agendar />,
       "Contacto": <Contacto />,
+      "Reporte": <Reporte />
     }
 
     useEffect(() => {
@@ -45,6 +47,17 @@ export default function Body() {
                     style={{color: page === "Agendar" ? "#00A3E0" : "", textDecoration: page === "Agendar" ? "underline" : ""}}
                     onClick={() => setPage("Agendar")}
                     ><strong>AGENDAR EVENTO</strong></button>
+
+              
+                )}
+
+{isAdmin && ( // Solo mostramos esto si el usuario es un admin
+               
+
+                    <button
+                    style={{color: page === "Reporte" ? "#00A3E0" : "", textDecoration: page === "Reporte" ? "underline" : ""}}
+                    onClick={() => setPage("Reporte")}
+                    ><strong>REPORTES</strong></button>
                 )}
 
                 <button // Boton de contacto
---
+++ src/Reporte.tsx
@@ -0,0 +1,19 @@
+import './Contacto.css'; // Importa tus estilos CSS
+
+export default function Contacto() {
+    return (
+        <div className='container'>
+            <div className='headLine'>
+                <h1>Contacto</h1>
+            </div><br/>
+            <div className="page">
+                <div style={{display:"flex", flexDirection: "column", justifyContent:"center", alignItems:"center"}}>
+                    <h2>Encuéntranos en:</h2>
+            
+      
+    
+                </div>
+            </div>
+        </div>
+    );
+}
