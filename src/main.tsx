32bf5c8dc74744f067af10d69c6a3758ba81f930
Gerardo Leiva Diaz <a01198479@tec.mx>
2024-08-18 15:15:44 -0600

    Usuario y signin
    Cambio de estilo para Signinbutton y usuario, al igual que ruta a inicio en SignOut

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ if(!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
       <App />
     </ClerkProvider>
   </React.StrictMode>,
